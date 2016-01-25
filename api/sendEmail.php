6e514cf213541c8ded4bdf42815b85bccc6583da
Dineth Hettiarachchi <dineth@sachintha.com>
2016-01-25 23:17:09 +0000

    bug fixes

--- api/sendEmail.php
+++ api/sendEmail.php
@@ -12,7 +12,35 @@
     $type = $_GET['type'];
     $interest = $_GET['interest'];
     
-    $message = $name."<br>".$emailAddress."<br>".$phone."<br>".$zipcode."<br>".$amount."<br>".$start."<br>".$type."<br>".$interest;
+    if ($amount == 0) {
+        $amount = "0-$500";
+    } elseif ($amount == 1) {
+        $amount = "$500-$1500";
+    } elseif ($amount == 2) {
+        $amount = "$1500-$2500";
+    } elseif ($amount == 3) {
+        $amount = "$2500 and above";
+    }
+    
+    if ($start == 0) {
+        $start = "0-3 months";
+    } elseif ($start == 1) {
+        $start = "3-6 months";
+    } elseif ($start == 2) {
+        $start = "6 months - 1 year";
+    }
+    
+    $message = "
+            Referer: $ref<br>
+            Name: $name<br>
+            Email Address: $emailAddress<br>
+            Phone #: $phone<br>
+            ZipCode: $zipcode<br>
+            Amount interested in investing: $amount<br>
+            When they want to get started: $start<br>
+            What type of marketing are they interested in: $type<br>
+            Why are they interested in Network Marketing: $interest<br>
+        ";
     
     
     $mail = new SimpleMail();
@@ -28,3 +56,5 @@
     $send = $mail->send();
     
     echo $send;
+    
+    //setTo('anna.melton79@hotmail.com', 'Anna Melton')
